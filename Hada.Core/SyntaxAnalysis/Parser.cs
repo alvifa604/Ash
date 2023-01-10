caccf7c8f98b11216659905342d14b378595394f
Alberto Villalobos <68524208+alvifa604@users.noreply.github.com>
2023-01-10 14:09:51 -0600

    Parser created.

---
+++ Hada.Core/SyntaxAnalysis/Parser.cs
@@ -0,0 +1,19 @@
+using Hada.Core.Errors;
+using Hada.Core.LexicalAnalysis;
+using Hada.Core.Text;
+
+namespace Hada.Core.SyntaxAnalysis;
+
+public class Parser
+{
+    public ErrorsBag ErrorsBag { get; } = new();
+    private readonly Token[] _tokens;
+
+    public Parser(SourceText source)
+    {
+        var lexer = new Lexer(source);
+        if (lexer.ErrorsBag.Any())
+            ErrorsBag.AddRange(lexer.ErrorsBag);
+        _tokens = lexer.GenerateTokens().ToArray();
+    }
+}
