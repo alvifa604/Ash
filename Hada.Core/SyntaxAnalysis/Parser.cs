eb66109953cbc779acffbd60bbf8487a6d8a125f
Alberto Villalobos <68524208+alvifa604@users.noreply.github.com>
2023-01-10 15:56:05 -0600

    Parser can parse binary expressions with numbers.

--- Hada.Core/SyntaxAnalysis/Parser.cs
+++ Hada.Core/SyntaxAnalysis/Parser.cs
@@ -1,6 +1,6 @@
 using Hada.Core.Errors;
 using Hada.Core.LexicalAnalysis;
-using Hada.Core.Text;
+using Hada.Core.SyntaxAnalysis.Expressions;
 
 namespace Hada.Core.SyntaxAnalysis;
 
@@ -8,12 +8,68 @@ internal sealed class Parser
 {
     public ErrorsBag ErrorsBag { get; } = new();
     private readonly Token[] _tokens;
+    private int _position;
 
-    public Parser(SourceText source)
+    private Token Current => _tokens[_position];
+
+    public Parser(Token[] tokens)
     {
-        var lexer = new Lexer(source);
-        if (lexer.ErrorsBag.Any())
-            ErrorsBag.AddRange(lexer.ErrorsBag);
-        _tokens = lexer.GenerateTokens().ToArray();
+        _tokens = tokens;
+        var endOfFileToken = _tokens.Last();
+        //MatchToken(TokenKind.EndOfFileToken);
     }
+
+    public Expression Parse()
+    {
+        return ParseExpression();
+    }
+
+    private Expression ParseExpression()
+    {
+        return ParseBinaryExpression(ParseTerm, TokenKind.PlusToken, TokenKind.MinusToken);
+    }
+
+    private Expression ParseTerm()
+    {
+        return ParseBinaryExpression(ParseFactor, TokenKind.MultiplicationToken, TokenKind.DivisionToken);
+    }
+
+    private Expression ParseBinaryExpression(Func<Expression> func, params TokenKind[] kinds)
+    {
+        var left = func();
+        while (kinds.Contains(Current.Kind))
+        {
+            var operatorToken = NextToken();
+            var right = func();
+            left = new BinaryExpression(left, operatorToken, right);
+        }
+
+        return left;
+    }
+
+    private Expression ParseFactor()
+    {
+        if (Current.Kind is TokenKind.IntegerToken or TokenKind.DoubleToken)
+        {
+            var numberToken = NextToken();
+            return new NumberExpression(numberToken);
+        }
+
+        return null;
+    }
+
+    private Token NextToken()
+    {
+        var current = Current;
+        _position++;
+        return current;
+    }
+
+    /*private Token MatchToken(TokenKind kind)
+    {
+        if (Current.Kind == kind) return NextToken();
+
+        ErrorsBag.ReportUnexpectedToken($"UnexpectedToken {Current.Kind} expected {kind}", Current.);
+        return new Token(kind, Current.Position, "", null);
+    }*/
 }
