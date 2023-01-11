3ee616a5900e3dad86bc2d74f5fda03a8c95dcf1
Alberto Villalobos <68524208+alvifa604@users.noreply.github.com>
2023-01-10 19:02:29 -0600

    Added position to token.

--- Hada.Core/LexicalAnalysis/Token.cs
+++ Hada.Core/LexicalAnalysis/Token.cs
@@ -5,12 +5,16 @@ public sealed class Token
     public TokenKind Kind { get; }
     public string Text { get; }
     public object? Value { get; }
+    public Position? Start { get; }
+    public Position? End { get; }
 
-    public Token(TokenKind kind, string text, object? value = null)
+    public Token(TokenKind kind, string text, object? value = null, Position? start = null, Position? end = null)
     {
         Kind = kind;
         Text = text;
         Value = value;
+        Start = start;
+        End = end;
     }
 
     public override string ToString()
