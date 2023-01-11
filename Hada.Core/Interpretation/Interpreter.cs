530756b4f04a4bd78905cc3ceb460cc8a7c0ba15
Alberto Villalobos <68524208+alvifa604@users.noreply.github.com>
2023-01-11 16:05:31 -0600

    Interpreter returns an interpreter result.

--- Hada.Core/Interpretation/Interpreter.cs
+++ Hada.Core/Interpretation/Interpreter.cs
@@ -8,79 +8,108 @@ namespace Hada.Core.Interpretation;
 internal sealed class Interpreter
 {
     private readonly SyntaxTree _tree;
-    public ErrorsBag ErrorsBag { get; } = new();
+    private readonly ErrorsBag _errorsBag = new();
 
     public Interpreter(SyntaxTree tree)
     {
         _tree = tree;
     }
 
-    public object? Interpret()
+    public InterpreterResult Interpret()
     {
         var result = Visit(_tree.Root!);
-        return result;
+        return new InterpreterResult(result, _errorsBag);
     }
 
-    private object Visit(Expression node)
+    private object? Visit(Expression node)
     {
-        return (node switch
+        return node switch
         {
             BinaryExpression binary => VisitBinary(binary),
             UnaryExpression unary => VisitUnary(unary),
             ParenthesizedExpression parenthesized => VisitParenthesized(parenthesized),
             //LiteralExpression literal => VisitLiteral(literal),
-            NumberExpression number => number.Token.Value,
-            _ => ErrorsBag.ReportInvalidExpression()
-        })!;
+            NumberExpression number => VisitNumber(number),
+            _ => VisitLiteral(node)
+        };
     }
 
-    private object VisitParenthesized(ParenthesizedExpression parenthesized)
+    private object VisitLiteral(Expression node)
+    {
+        throw new NotImplementedException();
+    }
+
+    private object? VisitNumber(NumberExpression number)
+    {
+        return number.Token.Value;
+    }
+
+    private object? VisitParenthesized(ParenthesizedExpression parenthesized)
     {
         return Visit(parenthesized.Expression);
     }
 
     private object? VisitUnary(UnaryExpression unary)
     {
-        var @operator = unary.OperatorToken.Kind;
-        dynamic expression = Visit(unary.Expression);
+        var @operator = unary.OperatorToken;
+        var expression = Visit(unary.Expression);
 
-        return @operator switch
+        switch (expression)
         {
-            TokenKind.PlusToken => expression,
-            TokenKind.MinusToken => -expression,
-            _ => throw new ArgumentOutOfRangeException(nameof(@operator), @operator, null)
-        };
+            case int:
+            case double:
+                dynamic number = expression;
+                return @operator.Kind switch
+                {
+                    TokenKind.PlusToken => number,
+                    TokenKind.MinusToken => -number,
+                    _ => throw new NotImplementedException()
+                };
+        }
+
+        _errorsBag.ReportInvalidUnaryOperator(@operator.Text, expression.GetType(), @operator.Start, @operator.End);
+        return expression;
     }
 
     private object? VisitBinary(BinaryExpression binary)
     {
-        dynamic left = Visit(binary.Left);
-        dynamic right = Visit(binary.Right);
+        var left = Visit(binary.Left);
+        var right = Visit(binary.Right);
 
-        dynamic? result;
-
-        switch (binary.OperatorToken.Kind)
+        if (left is (double or int) && right is (double or int))
         {
-            case TokenKind.PlusToken:
-                result = left + right;
-                break;
-            case TokenKind.MinusToken:
-                result = left - right;
-                break;
-            case TokenKind.MultiplicationToken:
-                result = left * right;
-                break;
-            case TokenKind.DivisionToken:
-                result = left / right;
-                break;
-            default:
-                ErrorsBag.ReportInvalidBinaryOperator(binary.OperatorToken);
-                result = null;
-                return result;
+            dynamic l = left;
+            dynamic r = right;
+            var result = l;
+            switch (binary.OperatorToken.Kind)
+            {
+                case TokenKind.PlusToken:
+                    result = l + r;
+                    break;
+                case TokenKind.MinusToken:
+                    result = l - r;
+                    break;
+                case TokenKind.MultiplicationToken:
+                    result = l * r;
+                    break;
+                case TokenKind.DivisionToken:
+                    if (r == 0)
+                    {
+                        _errorsBag.ReportDivisionByZero(binary.Right.Start, binary.Right.End);
+                        return null;
+                    }
+
+                    result = (double)l / r;
+                    break;
+            }
+
+            if (result is double res)
+                return Math.Round(res, 10);
+            return result;
         }
 
-        if (result is double res)
-            return Math.Round(res, 10);
-        return result;
+        _errorsBag.ReportInvalidBinaryOperator(binary.OperatorToken.Text, left.GetType(), right.GetType(),
+            binary.OperatorToken.Start, binary.OperatorToken.End);
+        return left;
     }
 }
