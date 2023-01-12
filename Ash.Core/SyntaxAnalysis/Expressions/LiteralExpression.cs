106f302c2890230d754957e042ae5fbc2731edca
Alberto Villalobos <68524208+alvifa604@users.noreply.github.com>
2023-01-12 14:44:02 -0600

    NumberExpression renamed to LiteralExpression.

--- Ash.Core/SyntaxAnalysis/Expressions/LiteralExpression.cs
+++ Ash.Core/SyntaxAnalysis/Expressions/LiteralExpression.cs
@@ -2,18 +2,24 @@ using Ash.Core.LexicalAnalysis;
 
 namespace Ash.Core.SyntaxAnalysis.Expressions;
 
-public class NumberExpression : Expression
+public class LiteralExpression : Expression
 {
     public Token Token { get; }
     public override TokenKind Kind => TokenKind.NumberExpression;
     public override Position Start { get; }
     public override Position End { get; }
+    public object? Value { get; }
 
-    public NumberExpression(Token token, Position start, Position end)
+    public LiteralExpression(Token token, Position start, Position end)
     {
         Token = token;
         Start = start;
         End = end;
+    }
+
+    public LiteralExpression(Token token, Position start, Position end, object value) : this(token, start, end)
+    {
+        Value = value;
     }
 
     public override string ToString()
